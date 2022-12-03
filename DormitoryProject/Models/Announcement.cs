ed1b05e2060acee058db8d057e63498e1867c148
Anxhulja <anxhulja.kurtaj7@gmail.com>
2022-12-03 19:19:48 +0100

    scaffold db

---
+++ DormitoryProject/Models/Announcement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DormitoryProject.Models
+{
+    public partial class Announcement
+    {
+        public Announcement()
+        {
+            Applications = new HashSet<Application>();
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public DateTime PublishedDate { get; set; }
+        public bool IsActive { get; set; }
+        public int RoomId { get; set; }
+
+        public virtual Room Room { get; set; } = null!;
+        public virtual ICollection<Application> Applications { get; set; }
+    }
+}
---
+++ DormitoryProject/Models/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DormitoryProject.Models
+{
+    public partial class Room
+    {
+        public Room()
+        {
+            Announcements = new HashSet<Announcement>();
+            RoomStudents = new HashSet<RoomStudent>();
+        }
+
+        public int Id { get; set; }
+        public string Code { get; set; } = null!;
+        public int Capacity { get; set; }
+        public int DormitoryId { get; set; }
+
+        public virtual Dormitory Dormitory { get; set; } = null!;
+        public virtual ICollection<Announcement> Announcements { get; set; }
+        public virtual ICollection<RoomStudent> RoomStudents { get; set; }
+    }
+}
